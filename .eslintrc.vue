49c2c2b6496a9c070afe8827309dfce78557b42d
Fritz <livsant123@gmail.com>
2019-01-31 18:46:19 +0300

    eslint vue, code settings

---
+++ .eslintrc.vue
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  plugins: [
+    'vue', 'prettier'
+  ],
+  extends: [
+    'plugin:vue/recommended', 'plugin:prettier/recommended'
+  ],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production"
+      ? "error"
+      : "off",
+    "no-debugger": process.env.NODE_ENV === "production"
+      ? "error"
+      : "off",
+    'prettier/prettier': [
+      'error', {
+        "trailingComma": "all",
+        "singleQuote": true,
+        "printWidth": 120
+      }
+    ]
+  },
+  parserOptions: {
+    parser: "babel-eslint"
+  }
+};
