d4cf9d2ca320d9cb858b79ef64da594f4a6db01c
fedor <fs@overgear.com>
2021-09-24 22:09:00 +0300

    force

--- dotsInstall.sh
+++ dotsInstall.sh
@@ -1,5 +1,25 @@
-#!/usr/bin/env zsh
-for package in stow/
-do
-	stow -vn -t ~/ $package
-done
+#!/bin/sh
+
+while getopts ":f" o; do case "${o}" in
+	f) force=true;;
+	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit 1 ;;
+esac done
+
+installStow() {
+	echo $force
+	for package in stow/
+	do
+		stow -vn -t ~/ $package  >> tmp 2>&1
+	done
+	cat tmp
+}
+
+applyForce() {
+	echo "FUCK!"
+	grep existing tmp | awk '{print $11}' | xargs -I {} rm ~/{}
+	installStow
+}
+
+installStow
+[ -n "$force" ] && applyForce
+rm tmp
--- readme.md
+++ readme.md
@@ -1,2 +1,19 @@
-This is my settings for some stuff like linter, editor etc.
-Could be changed any time, could be super bad.
+# My dotfiles
+zsh
+nvim
+ranger
+i3
+and other useful things
+
+# How to install
+Do fucking backup!!!
+Stow must be installed in order the script to work
+clone repo
+run
+```
+chmod +x dotsInstall.sh
+sh dotsInstall.sh
+```
+
+use ```-f``` option to delete existing dotfiles if there are already something conflicting.
+
